00897337ecfcf0a14073ff9d762551e44e1aa2b5
Sviatoslav Murzin <mursvyats@gmail.com>
2025-04-09 13:16:39 +0500

    fix: edited config for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [deno(), react()],
+  base: '/murzini/',
+  build: {
+    outDir: 'dist'
+  }
 })
